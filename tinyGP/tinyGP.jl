31028ffed678b84d9b7b4bb1b0a270752e84272e
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2025-10-07 14:10:14 +0200

    Refactoring

--- tinyGP/tinyGP.jl
+++ tinyGP/tinyGP.jl
@@ -1,5 +1,14 @@
 module TinyGP
 
+# TODO 
+# - parameters in the code instead of ERC
+# - AutoDiff
+# - read from CSV (and automatically determine num vars and num obs)
+# - postfix instead of prefix
+# - move PC out of Algorithm type (local var sufficient)
+# - parallel evaluation
+
+
 using Random
 using Printf
 
@@ -10,8 +19,8 @@ const DIV::UInt8 = 113
 const FSET_START = ADD
 const FSET_END = DIV
 
-const MAX_LEN = 10_000
-const POPSIZE = 100_000
+const MAX_LEN = 100
+const POPSIZE = 10_000
 const DEPTH = 5
 const GENERATIONS = 100
 const TSIZE = 2
@@ -20,35 +29,38 @@ const CROSSOVER_PROB = 0.9
 const BUFFER = Vector{UInt8}(undef, MAX_LEN)
 
 mutable struct Algorithm{T}
-    fitness::Vector{T}
-    pop::Vector{Vector{UInt8}}
-    rng::AbstractRNG
-    x::Vector{T}
-    minrandom::T
-    maxrandom::T
+    const fitness::Vector{T}
+    const pop::Vector{Vector{UInt8}}
+    const rng::AbstractRNG
+    const x::Vector{T}
+    const minrandom::T
+    const maxrandom::T
     program::Vector{UInt8}
-    pc::Int
-    varnumber::Int
-    fitnesscases::Int
-    randomnumber::Int
-    targets::Matrix{T}
+    pc::Int32
+    const varnumber::Int32
+    const fitnesscases::Int32
+    const randomnumber::Int32
+    const targets::Matrix{T}
     fbestpop::T
     favgpop::T
     avg_len::T
-    seed::Int
+    const seed::Int64
+    const generations::Int32
+    const maxlen::Int32
+    const tournamentsize::Int32
 end
 
-function Algorithm{T}(fname::AbstractString, seed::Integer=-1) where {T <: AbstractFloat}
+function Algorithm{T}(fname::AbstractString; seed=-1, generations=GENERATIONS, popsize=POPSIZE, maxlen=MAX_LEN, tournamentsize=TSIZE) where {T <: AbstractFloat}
     rng = seed >= 0 ? MersenneTwister(seed) : MersenneTwister()
     varnumber, randomnumber, minrandom, maxrandom, fitnesscases, targets = setup_fitness(T, fname)
     varnumber + randomnumber < FSET_START || error("too many variables and constants")
     
-    fitness = Vector{T}(undef, POPSIZE)
-    pop = Vector{Vector{UInt8}}(undef, POPSIZE)
+    fitness = Vector{T}(undef, popsize)
+    pop = Vector{Vector{UInt8}}(undef, popsize)
     x = [(maxrandom - minrandom) * rand(rng) + minrandom for _ in 1:Int(FSET_START)]
     gp = Algorithm{T}(fitness, pop, rng, x, minrandom, maxrandom, UInt8[], 0,
-                varnumber, fitnesscases, randomnumber, targets, 0.0, 0.0, 0.0, Int(seed))
-    gp.pop = create_random_pop!(gp, POPSIZE, DEPTH)
+                varnumber, fitnesscases, randomnumber, targets, 0.0, 0.0, 0.0, Int(seed), generations, maxlen, tournamentsize)
+    create_random_pop!(gp, popsize, DEPTH)
     return gp
 end
 
@@ -75,7 +87,7 @@ function setup_fitness(::Type{T}, fname::AbstractString) where {T <: AbstractFlo
     end
 end
 
-function traverse(buffer::Vector{UInt8}, pos::Int)
+function traverse(buffer, pos)
     primitive = buffer[pos + 1]
     if primitive < FSET_START
         return pos + 1
@@ -85,7 +97,7 @@ function traverse(buffer::Vector{UInt8}, pos::Int)
     end
 end
 
-function grow!(gp::Algorithm, buffer::Vector{UInt8}, pos::Int, maxlen::Int, depth::Int)
+function grow!(gp, buffer, pos, maxlen, depth)
     pos >= maxlen && return -1
     prim = rand(gp.rng, 0:1)
     if pos == 0
@@ -96,7 +108,7 @@ function grow!(gp::Algorithm, buffer::Vector{UInt8}, pos::Int, maxlen::Int, dept
         buffer[pos + 1] = UInt8(code)
         return pos + 1
     else
-        func = UInt8(rand(gp.rng, Int(FSET_START):Int(FSET_END)))
+        func = UInt8(rand(gp.rng, FSET_START:FSET_END))
         buffer[pos + 1] = func
         child = grow!(gp, buffer, pos + 1, maxlen, depth - 1)
         child < 0 && return -1
@@ -104,36 +116,35 @@ function grow!(gp::Algorithm, buffer::Vector{UInt8}, pos::Int, maxlen::Int, dept
     end
 end
 
-function create_random_indiv(gp::Algorithm, depth::Int)
-    len = grow!(gp, BUFFER, 0, MAX_LEN, depth)
+function create_random_indiv!(gp, depth)
+    len = grow!(gp, BUFFER, 0, gp.maxlen, depth)
     while len < 0
-        len = grow!(gp, BUFFER, 0, MAX_LEN, depth)
+        len = grow!(gp, BUFFER, 0, gp.maxlen, depth)
     end
-    ind = Vector{UInt8}(undef, len)
-    copyto!(ind, 1, BUFFER, 1, len)
-    return ind
+    
+    copyto!(Vector{UInt8}(undef, len), 1, BUFFER, 1, len)
 end
 
-function create_random_pop!(gp::Algorithm, n::Int, depth::Int)
-    pop = Vector{Vector{UInt8}}(undef, n)
-    for i in 1:n
-        pop[i] = create_random_indiv(gp, depth)
-        gp.fitness[i] = fitness_function(gp, pop[i])
+function create_random_pop!(gp, popsize, depth)
+    for i in 1:popsize
+        gp.pop[i] = create_random_indiv!(gp, depth)
+        gp.fitness[i] = fitness_function(gp, gp.pop[i])
     end
-    return pop
+    
+    gp.pop
 end
 
-function run_program!(gp::Algorithm, prog::Vector{UInt8})
+function run_program!(gp, prog)
     gp.program = prog
     gp.pc = 0
-    return eval_node!(gp)
+    eval_node!(gp)
 end
 
-function eval_node!(gp::Algorithm)
+function eval_node!(gp)
     primitive = gp.program[gp.pc + 1]
     gp.pc += 1
     if primitive < FSET_START
-        return gp.x[Int(primitive) + 1]
+        return gp.x[primitive + 1]
     else
         if primitive == ADD
             return eval_node!(gp) + eval_node!(gp)
@@ -155,7 +166,7 @@ function eval_node!(gp::Algorithm)
     end
 end
 
-function fitness_function(gp::Algorithm, prog::Vector{UInt8})
+function fitness_function(gp, prog)
     fit = 0.0
     for i in 1:gp.fitnesscases
         for j in 1:gp.varnumber
@@ -164,16 +175,16 @@ function fitness_function(gp::Algorithm, prog::Vector{UInt8})
         result = run_program!(gp, prog)
         fit += abs(result - gp.targets[i, gp.varnumber + 1])
     end
-    return -fit
+    -fit
 end
 
-function print_indiv(gp::Algorithm, buffer::Vector{UInt8}, pos::Int=0)
+function print_indiv(gp, buffer, pos=0)
     primitive = buffer[pos + 1]
     if primitive < FSET_START
         if primitive < gp.varnumber
-            print("X", Int(primitive) + 1, " ")
+            print("X", primitive + 1, " ")
         else
-            print(gp.x[Int(primitive) + 1])
+            print(gp.x[primitive + 1])
         end
         return pos + 1
     else
@@ -194,55 +205,53 @@ function print_indiv(gp::Algorithm, buffer::Vector{UInt8}, pos::Int=0)
     end
 end
 
-function tournament(gp::Algorithm, tsize::Int)
-    best = rand(gp.rng, 1:POPSIZE)
+function tournament!(gp)
+    popsize=length(gp.pop)
+    bestidx = rand(gp.rng, 1:popsize)
     fbest = -1.0e34
-    for _ in 1:tsize
-        competitor = rand(gp.rng, 1:POPSIZE)
+    for _ in 1:gp.tournamentsize
+        competitor = rand(gp.rng, 1:popsize)
         if gp.fitness[competitor] > fbest
             fbest = gp.fitness[competitor]
-            best = competitor
+            bestidx = competitor
         end
     end
-    return best
+    bestidx
 end
 
-function negative_tournament(gp::Algorithm, tsize::Int)
-    worst = rand(gp.rng, 1:POPSIZE)
+function negative_tournament!(gp)
+    popsize=length(gp.pop)
+    worstidx = rand(gp.rng, 1:popsize)
     fworst = 1.0e34
-    for _ in 1:tsize
-        competitor = rand(gp.rng, 1:POPSIZE)
+    for _ in 1:gp.tournamentsize
+        competitor = rand(gp.rng, 1:popsize)
         if gp.fitness[competitor] < fworst
             fworst = gp.fitness[competitor]
-            worst = competitor
+            worstidx = competitor
         end
     end
-    return worst
+    worstidx
 end
 
-function slice0(buffer::Vector{UInt8}, start0::Int, end0::Int)
-    if end0 <= start0
-        return UInt8[]
-    else
-        return buffer[start0 + 1:end0]
-    end
-end
-
-function crossover(gp::Algorithm, parent1::Vector{UInt8}, parent2::Vector{UInt8})
+function crossover(gp, parent1, parent2)
     len1 = traverse(parent1, 0)
     len2 = traverse(parent2, 0)
     xo1start = rand(gp.rng, 0:len1 - 1)
     xo1end = traverse(parent1, xo1start)
     xo2start = rand(gp.rng, 0:len2 - 1)
     xo2end = traverse(parent2, xo2start)
-    offspring = UInt8[]
-    append!(offspring, slice0(parent1, 0, xo1start))
-    append!(offspring, slice0(parent2, xo2start, xo2end))
-    append!(offspring, slice0(parent1, xo1end, len1))
-    return offspring
+    p1len = xo1start
+    p2len = xo2end - xo2start
+    p3len = len1 - xo1end
+    
+    offspring = Vector{UInt8}(undef,  p1len + p2len + p3len)
+    copyto!(offspring, 1,                 parent1, 1, p1len)
+    copyto!(offspring, 1 + p1len,         parent2, xo2start + 1, p2len)
+    copyto!(offspring, 1 + p1len + p2len, parent1, xo1end + 1, p3len)
+    offspring
 end
 
-function mutation(gp::Algorithm, parent::Vector{UInt8}, pmut::Float64)
+function mutate!(gp, parent, pmut)
     len = traverse(parent, 0)
     child = copy(parent)
     for i in 0:len - 1
@@ -254,15 +263,16 @@ function mutation(gp::Algorithm, parent::Vector{UInt8}, pmut::Float64)
             end
         end
     end
-    return child
+    child
 end
 
-function stats(gp::Algorithm, gen::Int)
-    best = rand(gp.rng, 1:POPSIZE)
+function update_stats!(gp, gen)
+    popsize = length(gp.pop)
+    best = rand(gp.rng, 1:popsize)
     gp.fbestpop = gp.fitness[best]
     gp.favgpop = 0.0
     node_count = 0
-    for i in 1:POPSIZE
+    for i in 1:popsize
         node_count += traverse(gp.pop[i], 0)
         gp.favgpop += gp.fitness[i]
         if gp.fitness[i] > gp.fbestpop
@@ -270,51 +280,53 @@ function stats(gp::Algorithm, gen::Int)
             gp.fbestpop = gp.fitness[i]
         end
     end
-    gp.avg_len = node_count / POPSIZE
-    gp.favgpop /= POPSIZE
+    gp.avg_len = node_count / popsize
+    gp.favgpop /= popsize
     @printf "Generation=%d Avg Fitness=%f Best Fitness=%f Avg Size=%f\nBest Individual: " gen -gp.favgpop -gp.fbestpop gp.avg_len
     print_indiv(gp, gp.pop[best])
     println()
     flush(stdout)
 end
 
-function print_parms(gp::Algorithm)
+function print_parms(gp)
+    popsize = length(gp.pop)
     @printf("-- TINY GP (Julia version) --\n")
     @printf("SEED=%d\nMAX_LEN=%d\nPOPSIZE=%d\nDEPTH=%d\nCROSSOVER_PROB=%f\n\
             PMUT_PER_NODE=%f\nMIN_RANDOM=%f\nMAXRANDOM=%f\nGENERATIONS=%d\n\
             TSIZE=%d\n----------------------------------\n",
-            gp.seed, MAX_LEN, POPSIZE, DEPTH, CROSSOVER_PROB,
-            PMUT_PER_NODE, gp.minrandom, gp.maxrandom, GENERATIONS, TSIZE)
+            gp.seed, gp.maxlen, popsize, DEPTH, CROSSOVER_PROB,
+            PMUT_PER_NODE, gp.minrandom, gp.maxrandom, gp.generations, gp.tournamentsize)
 end
 
-function evolve!(gp::Algorithm)
+function evolve!(gp)
     print_parms(gp)
-    stats(gp, 0)
-    for gen in 1:GENERATIONS - 1
+    update_stats!(gp, 0)
+    popsize = length(gp.pop)
+    for gen in 1:gp.generations - 1
         if gp.fbestpop > -1e-5
             println("PROBLEM SOLVED")
             return
         end
-        for _ in 1:POPSIZE
+        for _ in 1:popsize
             newind = if rand(gp.rng) < CROSSOVER_PROB
-                parent1 = tournament(gp, TSIZE)
-                parent2 = tournament(gp, TSIZE)
+                parent1 = tournament!(gp)
+                parent2 = tournament!(gp)
                 crossover(gp, gp.pop[parent1], gp.pop[parent2])
             else
-                parent = tournament(gp, TSIZE)
-                mutation(gp, gp.pop[parent], PMUT_PER_NODE)
+                parent = tournament!(gp)
+                mutate!(gp, gp.pop[parent], PMUT_PER_NODE)
             end
             newfit = fitness_function(gp, newind)
-            offspring = negative_tournament(gp, TSIZE)
+            offspring = negative_tournament!(gp)
             gp.pop[offspring] = newind
             gp.fitness[offspring] = newfit
         end
-        stats(gp, gen)
+        update_stats!(gp, gen)
     end
     println("PROBLEM *NOT* SOLVED")
 end
 
-function main(args::Vector{String})
+function main(args)
     fname = "problem.dat"
     seed = -1
     if length(args) == 2
@@ -330,5 +342,11 @@ end
 if abspath(PROGRAM_FILE) == @__FILE__
     main(ARGS)
 end
+
+# only for testing
+gp = Algorithm{Float64}("problem.dat", seed=3141, generations=2, popsize=1000)
+evolve!(gp)
+@assert (@show gp.favgpop) == -353.1842657981622
+@assert (@show gp.fbestpop) == -32.17519321627807
 
 end # module
