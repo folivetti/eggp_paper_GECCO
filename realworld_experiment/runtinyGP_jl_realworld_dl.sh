c37c3a9bc82d35c9b6bb3fd9fd0f005c99001122
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2025-10-10 18:01:11 +0200

    Change configuration to improve speed of experiments

--- realworld_experiment/runtinyGP_jl_realworld_dl.sh
+++ realworld_experiment/runtinyGP_jl_realworld_dl.sh
@@ -2,14 +2,14 @@
 
 mkdir -p results/tinyGP_jl_dl/$1
 
-GEN=100
+GEN=50
 POP=500
-TSIZE=5
+TSIZE=3
 LEN=50
 
-for i in {1..30};
+for i in {1..10};
 do
-    { time ~/julia/julia -t 64 --project=../tinyGP/ ../tinyGP/runTinyGP.jl datasets/$1_train.csv target \
+    { time ~/julia/julia -t 12 --project=../tinyGP/ ../tinyGP/runTinyGP.jl datasets/$1_train.csv target \
       -g $GEN -p $POP -t $TSIZE -m $LEN --objective=dl --test=datasets/$1_test.csv \
       > results/tinyGP_jl_dl/$1/run_${i}.csv; }
 done  2> results/tinyGP_jl_dl/$1/time
