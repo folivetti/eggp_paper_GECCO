a23a0734e49eb617dd380ee229e2d0ffbba882f3
Olivetti <fabricio.olivetti@gmail.com>
2025-10-10 14:13:39 -0300

    qlattice

--- realworld_experiment/runQLattice_realworld.sh
+++ realworld_experiment/runQLattice_realworld.sh
@@ -4,7 +4,7 @@ mkdir -p results/qlattice/$1
 
 GEN=100000
 
-for i in {1..10};
+for i in {11..30};
 do
     { time python QLattice_realworld_wrapper.py $1 $GEN > results/qlattice/$1/run_${i}.csv; } 2>> results/qlattice/$1/time ;
 done
