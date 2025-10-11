a07c87839ec22d5ebc9afde877f3e817bdd92982
folivetti <fabricio.olivetti@gmail.com>
2025-10-11 06:44:22 -0300

    gpzd script

--- realworld_experiment/gen_dataframe.py
+++ realworld_experiment/gen_dataframe.py
@@ -47,7 +47,7 @@ elif grid == "pysips":
     ref = "PySIPS"
     base_dir = "PySIPS_grid/"
 else:
-    algs = ["symregg","eggp_mo",  "PySIPS", "random", "operon", "qlattice"] # "qlattice", "gpzgd", "Random", "RF"]
+    algs = ["symregg","eggp_mo",  "PySIPS", "random", "operon", "qlattice", "gomea"] # "qlattice", "gpzgd", "Random", "RF"]
     ref = "eggp_mo"
     base_dir = ""
 
