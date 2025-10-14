3841317c0cc8024c02582f1f59f257e70d0bb173
Olivetti <fabricio.olivetti@gmail.com>
2025-10-14 17:04:11 -0300

    qlattice fixed

--- realworld_experiment/runEggp_mo_realworld.sh
+++ realworld_experiment/runEggp_mo_realworld.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p results/eggp_mo/$1
+mkdir -p results/eggp_mo_iters/$1
 
 PM=0.1
 PC=0.9
@@ -9,6 +9,6 @@ POP=500
 
 for i in {1..30}; 
 do 
-	{ time eggp -d datasets/$1_train.csv:::target --test datasets/$1_test.csv:::target -g $GEN --nPop $POP --pm $PM --pc $PC --tournament-size 2 -s 50 -k 2 --loss MSE --opt-iter 10 --opt-retries 1 --non-terminals add,sub,mul,div,exp,log,sin,power,sqrt --simplify > results/eggp_mo/$1/run_${i}.csv; } 2>> results/eggp_mo/$1/time
+	{ time eggp -d datasets/$1_train.csv:::target --test datasets/$1_test.csv:::target -g $GEN --nPop $POP --pm $PM --pc $PC --tournament-size 2 -s 50 -k 2 --loss MSE --opt-iter 50 --opt-retries 2 --non-terminals add,sub,mul,div,exp,log,sin,power,sqrt --simplify > results/eggp_mo_iters/$1/run_${i}.csv; } 2>> results/eggp_mo_iters/$1/time
 
 done
