d5365838f36300e391e550862bf68469eace278d
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2025-10-16 17:31:55 +0200

    Change scripts to use NeoGP

---
+++ realworld_experiment/runNeoGP_jl_realworld_nll.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p results/NeoGP_jl_nll/$1
+
+GEN=50
+POP=500
+TSIZE=3
+LEN=50
+
+for i in {1..10};
+do
+    { time ~/julia/julia -t 12 --project=../NeoGP/ ../NeoGP/runNeoGP.jl datasets/$1_train.csv target \
+      -g $GEN -p $POP -t $TSIZE -m $LEN --objective=nll --test=datasets/$1_test.csv \
+      > results/NeoGP_jl_nll/$1/run_${i}.csv; }
+done  2> results/NeoGP_jl_nll/$1/time
