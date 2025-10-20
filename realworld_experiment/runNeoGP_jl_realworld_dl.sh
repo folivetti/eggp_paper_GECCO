c410509349ed456fc749492359c1039c617e39b6
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2025-10-20 15:41:39 +0000

    Results for NeoGP dl fixedsigma friction stat and friction dyn

--- realworld_experiment/runNeoGP_jl_realworld_dl.sh
+++ realworld_experiment/runNeoGP_jl_realworld_dl.sh
@@ -2,7 +2,7 @@
 
 mkdir -p results/NeoGP_jl_dl_fixedsigma/$1
 
-GEN=50
+GEN=200
 POP=500
 TSIZE=3
 LEN=100
@@ -11,5 +11,5 @@ for i in {1..10};
 do
     { time ~/julia/julia -t 6 --project=../NeoGP/ ../NeoGP/runNeoGP.jl datasets/$1_train.csv target \
       -g $GEN -p $POP -t $TSIZE -m $LEN --objective=dl --sigma $2 --test=datasets/$1_test.csv \
-      > results/NeoGP_jl_dl_fixedsigma/$1/run_${i}.csv; }
+      > results/NeoGP_jl_dl_fixedsigma/$1/run_${i}.csv & }
 done  2> results/NeoGP_jl_dl_fixedsigma/$1/time
