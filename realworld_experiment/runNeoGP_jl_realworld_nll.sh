721c9f1595e365ac028670ae74c5e7dc94ade087
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2025-10-24 11:25:28 +0000

    More runs for Nikuradse 1 nll and dl.

--- realworld_experiment/runNeoGP_jl_realworld_nll.sh
+++ realworld_experiment/runNeoGP_jl_realworld_nll.sh
@@ -2,14 +2,14 @@
 
 mkdir -p results/NeoGP_jl_nll_fixedsigma/$1
 
-GEN=300
+GEN=200
 POP=500
 TSIZE=3
-LEN=200
+LEN=100
 
 for i in {1..10};
 do
     { time ~/julia/julia -t 6 --project=../NeoGP/ ../NeoGP/runNeoGP.jl datasets/$1_train.csv target \
       -g $GEN -p $POP -t $TSIZE -m $LEN --objective=nll --sigma $2 --test=datasets/$1_test.csv \
-      > results/NeoGP_jl_nll_fixedsigma/$1/run_${i}.csv; }
+      > results/NeoGP_jl_nll_fixedsigma/$1/run_${i}.csv & }
 done  2> results/NeoGP_jl_nll_fixedsigma/$1/time
