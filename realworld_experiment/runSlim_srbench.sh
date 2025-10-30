f95db25c04b3ae7694c2bf44bce99a6b0361dda0
folivetti <fabricio.olivetti@gmail.com>
2025-10-30 18:06:58 -0300

    GSGP and RF for realworld

--- realworld_experiment/runSlim_srbench.sh
+++ realworld_experiment/runSlim_srbench.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-mkdir -p results/slim_gsgp_10000/$1
+mkdir -p results/slim_gsgp/$1
 
 GEN=10000
 POP=500
 
-for FOLD in {0..2}; 
-do
-
-for i in {1..10}; 
+for i in {1..30};
 do 
-    { time python GSGP_srbench_wrapper.py $1 $GEN $POP $FOLD > results/slim_gsgp_10000/$1/run_${i}_${FOLD}.csv; } 2>> results/slim_gsgp_10000/$1/time ;
-
-done
+    { time python GSGP_srbench_wrapper.py $1 $GEN $POP > results/slim_gsgp_10000/$1/run_${i}.csv; } 2>> results/slim_gsgp_10000/$1/time ;
 done
