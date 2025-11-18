Use unprotected symbols for operon and fix issue with linear scaling terms.

experiments/Operon_wrapper.py
@@ -1,6 +1,6 @@
-import sys 
-import numpy as np 
-import pandas as pd 
+import sys
+import numpy as np
+import pandas as pd
 from pyoperon.sklearn import SymbolicRegressor
 from sklearn.metrics import r2_score, make_scorer, mean_squared_error
 from pyoperon import R2, MSE, InfixFormatter, FitLeastSquares, Interpreter
@@ -8,7 +8,7 @@ import sympy
 
 
 reg = SymbolicRegressor(
-        allowed_symbols= "add,sub,mul,div,exp,logabs,pow,sqrtabs,abs,sin,constant,variable",
+        allowed_symbols= "add,sub,mul,div,exp,log,pow,sqrt,abs,sin,constant,variable",
         crossover_probability= 0.9,
         mutation_probability= 0.3,
         female_selector= "tournament",
@@ -24,8 +24,8 @@ reg = SymbolicRegressor(
         reinserter= "keep-best",
         tournament_size= 5,
         max_evaluations=int(1e10),
-        add_model_scale_term = False,
-        add_model_intercept_term = False,
+        add_model_scale_term = True,
+        add_model_intercept_term = True,
         )
 
 if len(sys.argv) > 2:
@@ -42,10 +42,10 @@ reg.fit(X_train, y_train)
 
 print("Id,Expression,size,MSE_train,MSE_test,nll_train,nll_test,R2_train,R2_test")
 for i, model in enumerate(reg.pareto_front_):
-    print(X_train.shape, model['model'])
+    # print(X_train.shape, model['model'])
     y_hat = reg.evaluate_model(model['tree'], X_train)
     y_hat_test = reg.evaluate_model(model['tree'], X_test)
-    
+
     mse_train = mse_test = r2_train = r2_test = 0.0
     if np.any(np.isnan(y_hat)):
         mse_train = np.inf
@@ -53,7 +53,7 @@ for i, model in enumerate(reg.pareto_front_):
     else:
         mse_train = mean_squared_error(y_train, y_hat)
         r2_train = r2_score(y_train,y_hat)
-    
+
     if np.any(np.isnan(y_hat_test)):
         mse_test = np.inf
         r2_test = np.inf
@@ -61,3 +61,4 @@ for i, model in enumerate(reg.pareto_front_):
         mse_test = mean_squared_error(y_test,y_hat_test)
         r2_test = r2_score(y_test,y_hat_test)
     print(f"0,{model['model']},{model['complexity']},{mse_train},{mse_test},{mse_train},{mse_test},{r2_train},{r2_test}")
+