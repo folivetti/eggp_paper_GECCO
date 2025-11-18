0e4c60274759807442a7b8168a85dfc0c38715b2
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2025-11-18 08:32:44 +0000

    Upcase target folder

--- experiments/runOperon.sh
+++ experiments/runOperon.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-mkdir -p ../report/results/operon/$1
+mkdir -p ../report/results/Operon/$1
 
 if [ "$2" = "srbench" ]; then
     for FOLD in {0..2};
     do
         for i in {1..10};
         do
-            { time python Operon_wrapper.py $1 $FOLD > ../report/results/operon/$1/run_${i}_${FOLD}.csv; } 2>> ../report/results/operon/$1/time
+            { time python Operon_wrapper.py $1 $FOLD > ../report/results/Operon/$1/run_${i}_${FOLD}.csv; } 2>> ../report/results/Operon/$1/time
         done 
     done
 else
     for i in {1..30};
     do
-        { time python Operon_wrapper.py $1 > ../report/results/operon/$1/run_${i}.csv; } 2>> ../report/results/operon/$1/time
+        { time python Operon_wrapper.py $1 > ../report/results/Operon/$1/run_${i}.csv; } 2>> ../report/results/Operon/$1/time
     done
 fi
 
