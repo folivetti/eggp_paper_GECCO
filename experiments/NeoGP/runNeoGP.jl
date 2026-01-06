236e7d75a4246068cbe2e929233f1f053a4d6e17
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2026-01-06 15:22:41 +0100

    Adapt scripts to work with new neogp version

--- experiments/NeoGP/runNeoGP.jl
+++ experiments/NeoGP/runNeoGP.jl
@@ -2,6 +2,7 @@ using ArgParse
 using TimerOutputs
 using DelimitedFiles
 using NeoGP
+using LinearAlgebra
 
 function main(argv)
     s = ArgParseSettings()
@@ -14,12 +15,17 @@ function main(argv)
             help = "Number of generations"
             arg_type = Int
             default = 100
-        "--popsize", "-p"
-            help = "Population size"
+        "--batchsize", "-b"
+            help = "Batch size for fitness evaluations"
             arg_type = Int
             default = 100
+        "--popsize", "-p"
+            help = "Population size for tournament selection. MAP-Elites uses a variable-size archive of solution candidates. 
+            This parameter creates a virtual population of the given size by selecting randomly from the archive for tournament selection."
+            arg_type = Int
+            default = 1000
         "--tournamentsize", "-t"
-            help = "Tournament size."
+            help = "Tournament size for selection"
             arg_type = Int
             default = 2
         "--maxlen", "-m"
@@ -30,8 +36,8 @@ function main(argv)
             help = "Objective"
             default = "mse"
         "--sigma" # TODO support different likelihoods
-            help = "Sigma value for Gaussian likelihood (only used if applicable, e.g., nll or dl objective)"
-            arg_type = Float32
+            help = "Sigma value for Gaussian likelihood (only used if applicable, e.g., nll or dl objective). Can be a double value or a variable from the datasets"
+
         "--threads"
             help = "Maximum number of parallel threads"
             arg_type = Int
@@ -39,95 +45,136 @@ function main(argv)
         "--test"
             help = "Test dataset file in CSV format (defaults to training file)"
             default = ""
+        # TODO: different algorithms
     end
     
     parsed = parse_args(argv, s; as_symbols=false)
     if !haskey(parsed, "training") || !haskey(parsed, "target")
-        println("Usage: NeoGP.jl trainingdata.csv targetvariable [--generations N] [--popsize N] [--tournamentsize N] [--maxlen N] [--objective OBJ] [--testdataset FILE]")
+        println("Usage: NeoGP.jl trainingdata.csv targetvariable [--generations N] [--batchsize N] [--popsize N] [--tournamentsize N] [--maxlen N] [--objective OBJ] [--testdataset FILE]")
         return
     end
 
     trainingfilename = parsed["training"]
     targetname = parsed["target"]
     generations = parsed["generations"]
+    batchsize = parsed["batchsize"]
     popsize = parsed["popsize"]
-    tsize = parsed["tournamentsize"]
+    tournamentsize = parsed["tournamentsize"]
     maxlen = parsed["maxlen"]
-    objective = parsed["objective"]
-    sigma = parsed["sigma"]
+    objective = lowercase(parsed["objective"])
+    sigmastr = parsed["sigma"]
     nthreads = parsed["threads"]
     testdataset = parsed["test"] == "" ? trainingfilename : parsed["test"]
 
-    X, y = load_dataset(Float32, trainingfilename, targetname)
-    X_test, y_test = load_dataset(Float32, testdataset, targetname)
+    
+    if !isnothing(sigmastr)
+        sigma_val = tryparse(Float32, sigmastr)
+        if isnothing(sigma_val)
+            X, y, sigma = load_dataset(Float32, trainingfilename, targetname, sigmastr)
+            X_test, y_test, sigma_test = load_dataset(Float32, testdataset, targetname, sigmastr)
+        else
+            sigma = sigma_test = sigma_val
+            X, y = load_dataset(Float32, trainingfilename, targetname)
+            X_test, y_test = load_dataset(Float32, testdataset, targetname)
+        end
+    else
+        sigma = sigma_test = nothing
+        X, y = load_dataset(Float32, trainingfilename, targetname)
+        X_test, y_test = load_dataset(Float32, testdataset, targetname)
+    end
+    
+    functionset = Set([NeoGP.ADD, NeoGP.SUB, NeoGP.MUL, NeoGP.DIV, NeoGP.SIN, NeoGP.EXP, NeoGP.LOGABS, NeoGP.SQRTABS, NeoGP.POWABS])
 
     if objective == "mse"
         if !isnothing(sigma)
             @warn "sigma argument is ignored when using mse objective"
         end
-        likelihood = NeoGP.GaussianLikelihood(X, y)
+        likelihood = NeoGP.GaussianLikelihood(X, y) # no advantage to fix sigma here because it is calculated implicitly from residuals anyway
         likelihood_test = NeoGP.GaussianLikelihood(X_test, y_test)
-        loss_func = NeoGP.mean_squared_error
+        loss_func = (NeoGP.negloglik, NeoGP.individual_length)
+        model_selection_obj = NeoGP.mean_squared_error
     elseif objective == "r2"
         if !isnothing(sigma)
             @warn "sigma argument is ignored when using mse objective"
         end
-        likelihood = NeoGP.GaussianLikelihood(X, y)
+        likelihood = NeoGP.GaussianLikelihood(X, y)  # no advantage to fix sigma here because it is calculated implicitly from residuals anyway
         likelihood_test = NeoGP.GaussianLikelihood(X_test, y_test)
-        loss_func = ((-) ∘ NeoGP.r2_score)
+        loss_func = (NeoGP.negloglik, NeoGP.individual_length)
+        model_selection_obj = (-) ∘ NeoGP.r2_score
     elseif objective == "nll"
-        # TODO allow specification of different likelihoods
-        likelihood = NeoGP.GaussianLikelihood(X, y, sigma) # optimize sigma
-        likelihood_test = NeoGP.GaussianLikelihood(X_test, y_test, sigma)
-        loss_func = NeoGP.negloglik
+        likelihood = NeoGP.GaussianLikelihood(X, y, sigma)
+        likelihood_test = NeoGP.GaussianLikelihood(X_test, y_test, sigma_test)
+        loss_func = (NeoGP.negloglik, NeoGP.individual_length)
+        model_selection_obj = NeoGP.negloglik
+    elseif objective == "aic"
+        likelihood = NeoGP.GaussianLikelihood(X, y, sigma)
+        likelihood_test = NeoGP.GaussianLikelihood(X_test, y_test, sigma_test)
+        loss_func = (NeoGP.negloglik, NeoGP.individual_length)
+        model_selection_obj = NeoGP.AIC
+    elseif objective == "bic"
+        likelihood = NeoGP.GaussianLikelihood(X, y, sigma)
+        likelihood_test = NeoGP.GaussianLikelihood(X_test, y_test, sigma_test)
+        loss_func = (NeoGP.negloglik, NeoGP.individual_length)
+        model_selection_obj = NeoGP.BIC
     elseif objective == "dl"
-        # TODO allow specification of different likelihoods
-        likelihood = NeoGP.GaussianLikelihood(X, y, sigma) # optimize sigma
-        likelihood_test = NeoGP.GaussianLikelihood(X_test, y_test, sigma)
-        loss_func = NeoGP.description_length
-    elseif objective == "nll-dl"
-        # TODO allow specification of different likelihoods
-        likelihood = NeoGP.GaussianLikelihood(X, y, sigma) # optimize sigma
-        likelihood_test = NeoGP.GaussianLikelihood(X_test, y_test, sigma)
-        loss_func = (params...) -> gp.gen < 0.35 * gp.maxgenerations ? NeoGP.negloglik(params...) : NeoGP.description_length(params...)
+        likelihood = NeoGP.GaussianLikelihood(X, y, sigma)
+        likelihood_test = NeoGP.GaussianLikelihood(X_test, y_test, sigma_test)
+        loss_func = (NeoGP.negloglik, NeoGP.description_length_complexity_penalty)
+        model_selection_obj = NeoGP.description_length
+    elseif objective == "fbf"
+        likelihood = NeoGP.GaussianLikelihood(X, y, sigma)
+        likelihood_test = NeoGP.GaussianLikelihood(X_test, y_test, sigma_test)
+        loss_func = (NeoGP.negloglik, NeoGP.fractional_bayes_factor_complexity_penalty)
+        model_selection_obj = NeoGP.fractional_bayes_factor
     else
-        error("unknown objective function value (allowed values are mse, r2, nll, dl)")
+        error("unknown objective function value (allowed values are mse, r2, nll, aic, bic, fbf, dl)")
     end
-    gp = NeoGP.Algorithm(likelihood,
-        generations = generations, popsize = popsize, maxlen = maxlen, tournamentsize = tsize,
-        loss_func = loss_func, threads = nthreads, 
-        functionset = Set([NeoGP.ADD, NeoGP.SUB, NeoGP.MUL, NeoGP.DIV, NeoGP.SIN, NeoGP.EXP, NeoGP.LOG, NeoGP.SQRT, NeoGP.POW]))
-
-    println("gen,fevals,best_fitness,dl,MSE_train,MSE_test,R2_train,R2_test,nll_train,nll_test,avg_len,avg_fitness,size,Expression")
+    # gp = NeoGP.MAPElitesAlgorithm(likelihood, tournamentsize = tournamentsize, popsize=popsize, batchsize = batchsize, maxlen = maxlen,
+    #     indiv_loss_func = loss_func, threads = nthreads, functionset = functionset)
+    # gp = NeoGP.BasicAlgorithm(likelihood, tournamentsize = tournamentsize, popsize=popsize, maxlen = maxlen,
+    #     indiv_loss_func = loss_func, threads = nthreads, functionset = functionset)
+    gp = NeoGP.NSGA2(likelihood, tournamentsize = tournamentsize, popsize=popsize, maxlen = maxlen,
+        indiv_loss_func = loss_func, threads = nthreads, functionset = functionset)
     gen = 0
+    println("gen,fevals,|bestfront|,bestloss,obj,nll,dl,func_compl,param_compl,FBF,MSE_train,MSE_test,R2_train,R2_test,size,expression")
+
+    buffers = NeoGP.InterpreterBuffers(Float32, size(X, 1), size(X, 2), maxlen)
     callback = () -> begin
         gen += 1
-        bestfitness,bestidx = findmax(gp.fitness)
-        bestindiv = gp.pop[bestidx]
+        
+        front = filter(tup -> NeoGP.individual_length(tup[1]) <= maxlen, NeoGP.best_individuals(gp))  # filter out individuals that are too long
+        
+        
+        bestobj, bestidx = findmin(tup -> model_selection_obj(tup[1], buffers), front)  # find individual with best model selection objective
+        bestindiv, bestloss = front[bestidx]
+        
+        param = NeoGP.extractparam(Float32, NeoGP.getprogram(bestindiv))
         best_expr_str = NeoGP.tostring(bestindiv)
-        ypred_train = NeoGP.predict(bestindiv, likelihood.X) 
-        ypred_test  = NeoGP.predict(bestindiv, X_test)
-        mse_train   = NeoGP.mean_squared_error(likelihood.y, ypred_train)
+        ypred_train = NeoGP.predict!(bestindiv, X, param, buffers) 
+        mse_train   = NeoGP.mean_squared_error(y, ypred_train)
+        r2_train    = NeoGP.r2_score(y, ypred_train)
+
+        ypred_test  = NeoGP.predict!(bestindiv, X_test, param, buffers)
         mse_test    = NeoGP.mean_squared_error(y_test, ypred_test)
-        r2_train    = NeoGP.r2_score(likelihood.y, ypred_train)
         r2_test     = NeoGP.r2_score(y_test, ypred_test)
-        nll_train   = NeoGP.negloglik(likelihood, ypred_train, bestindiv.likelihood.sigma_err)
-        nll_test    = NeoGP.negloglik(likelihood_test, ypred_test, bestindiv.likelihood.sigma_err)
         
-        # for evaluation of DL we use the likelihood with optimized sigma
-        # dl_likelihood = NeoGP.GaussianLikelihood(bestindiv.likelihood.X, bestindiv.likelihood.y, bestindiv.likelihood.sigma_err)
-        # bestindv_copy = NeoGP.Individual(copy!(similar(bestindiv.program),bestindiv.program) , dl_likelihood) # copy to avoid modifying the original individual
+        (nll, func_compl, param_compl) = NeoGP.description_length_terms(bestindiv, buffers)
+        dl = nll + func_compl + param_compl
         
-        dl            = NeoGP.description_length(NeoGP.copy(bestindiv))
+        fbf = NeoGP.fractional_bayes_factor(bestindiv, buffers)
         
-        println("$gen,$(gp.fevals),$(-bestfitness),$dl,$mse_train,$mse_test,$r2_train,$r2_test,$nll_train,$nll_test,$(gp.avg_len),$(-gp.favgpop),$(length(bestindiv.program)),\"$(best_expr_str)\"")
+        println("$gen,$(gp.fevals),$(length(front)),$(bestloss[1]),$(bestobj),$(nll),$(dl),$func_compl,$param_compl,$fbf,$mse_train,$mse_test,$r2_train,$r2_test,$(NeoGP.individual_length(bestindiv)),\"$(best_expr_str)\"")
         nothing
     end
-    TimerOutputs.disable_timer!(gp.to)
-    @time NeoGP.evolve!(gp, iter_callback = callback)
-    # TimerOutputs.print_timer(gp.to)
+    
+    TimerOutputs.disable_timer!(NeoGP.global_timer)
+    
+    # @time NeoGP.evolve!(gp) # without callback
+    NeoGP.initialize!(gp)
+    callback() # report for initial population
+    @time NeoGP.evolve!(gp, generations, iter_callback = callback)
+    
 end
-
 
 # all columns except for the target are allowed input
 function load_dataset(::Type{T}, filename::AbstractString, targetname) where {T <: AbstractFloat}
--- experiments/runNeoGP.sh
+++ experiments/runNeoGP.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 PNAME=$1
-mkdir -p ../report/results/neogp/$PNAME
+for pname in $PNAME; do
+  mkdir -p ../report/results/neogp_nsga2/$pname
+done
 
-NJOBS=10
-
-# without parallel for testing
-# ~/julia/julia -t 6 --project=NeoGP/ \
-# 	NeoGP/runNeoGP.jl datasets/${PNAME}_train.csv target -g $GEN -p $POP -t $TSIZE -m $LEN \
-# 	  --objective=nll --sigma 1.0 --test=datasets/${PNAME}_test.csv \
-# 	> ../report/results/neogp/${PNAME}/run_1.csv
-
-# nll with a fixed sigma (e.g. 1.0) is equivalent to optimizing mse (../report/results for nll and dl are however not useful)
+NJOBS=8
 
 if [ $2 = "srbench" ]; then
-    parallel -j$NJOBS ~/julia/julia -t 6 --project=NeoGP/ \
-        NeoGP/runNeoGP.jl datasets/srbench/${PNAME}_train{2}.csv target -g 200 -p 500 -t 2 -m 50 \
-          --objective=nll --sigma 1.0 --test=datasets/srbench/${PNAME}_test{2}.csv \
-        "> results/neogp/${PNAME}/run_{2}_{1}.csv" ::: $(seq 1 10) ::: $(seq 0 2)
+    parallel -S .. -j$NJOBS --return ~/eggp_paper_GECCO/report/results/neogp_nsga2/{3}/run_{2}_{1}.csv \
+         (cd ~/eggp_paper_GECCO/experiments/; \
+         mkdir -p ../report/results/neogp_nsga2/{3}; \
+         ~/julia/julia -t 10 --project=NeoGP/ \
+        NeoGP/runNeoGP.jl datasets/srbench/{3}_train{2}.csv target -g 200 -p 500 -t 2 -m 50 \
+          --objective=fbf --test=datasets/srbench/{3}_test{2}.csv \
+        "> ../report/results/neogp_nsga2/{3}/run_{2}_{1}.csv") ::: $(seq 1 10) ::: $(seq 0 2) ::: $PNAME
 else
-    parallel -j$NJOBS ~/julia/julia -t 6 --project=NeoGP/ \
-        NeoGP/runNeoGP.jl datasets/realworld/${PNAME}_train.csv target -g 200 -p 500 -t 2 -m 50 \
-          --objective=nll --sigma 1.0 --test=datasets/realworld/${PNAME}_test.csv \
-        "> ../report/results/neogp/${PNAME}/run_{1}.csv" ::: $(seq 1 30)
+    parallel -S .. -j$NJOBS --return ~/eggp_paper_GECCO/report/results/neogp_nsga2/{2}/run_{1}.csv \
+        (cd ~/eggp_paper_GECCO/experiments/; \
+         mkdir -p ../report/results/neogp_nsga2/{2}; \
+         ~/julia/julia -t 10 --project=NeoGP/ \
+        NeoGP/runNeoGP.jl datasets/realworld/{2}_train.csv target -g 200 -p 500 -t 2 -m 50 \
+          --objective=fbf --test=datasets/realworld/{2}_test.csv \
+        "> ../report/results/neogp_nsga2/{2}/run_{1}.csv") ::: $(seq 1 30) ::: $PNAME
 fi
